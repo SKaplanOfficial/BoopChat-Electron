83709e43df0949fa7913f6c8584dc9f49b4fd8c2
Stephen Kaplan <nintendopup@icloud.com>
2018-08-15 18:34:57 -0400

    Revert "Merge branch 'master' of https://github.com/SKaplanOfficial/BoopChat-Electron"
    This reverts commit 248da62fedf543971f3985befcc22ab6fb26c6e9, reversing
    changes made to 2df87b62c06f7e232f3fb7cf657463fb33c33226.

--- run.js
+++ run.js
@@ -2,7 +2,8 @@ const {app, BrowserWindow, Menu} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 400, height: 600, frame: true, icon:"favicon.png"})
+    win = new BrowserWindow({width: 400, height: 600, frame: true})
+
     // and load the index.html of the app.
     win.loadFile('index.html')
   }
