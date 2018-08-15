61bcc6eae6638beed947f79f92745c4a82f51796
That'sMyFace <20381559+thatsmyface@users.noreply.github.com>
2018-08-15 18:56:46 -0400

    let me keep my dang favicon

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow, Menu} = require('electron')
 
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 400, height: 600, frame: true})
+    win = new BrowserWindow({width: 400, height: 600, frame: true, icon: "favicon.png"})
 
     // and load the index.html of the app.
     win.loadFile('index.html')
